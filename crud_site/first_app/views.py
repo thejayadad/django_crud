91118305e65d9371c0b1a4b6679392355f4e10ba
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-19 12:05:05 -0400

    update

--- crud_site/first_app/views.py
+++ crud_site/first_app/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
+
 
 def index(request):
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
     my_dict = {'insert_me': 'Hello Im from views.py'}
-    return render(request,'index.html', context=my_dict)
+    return render(request,'index.html', context=date_dict)
